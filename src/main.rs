686975e90ca5e833911ce81ddca1a013e9508d3d
NetCreature <agustinmediotti@gmail.com>
2025-07-21 17:58:43 -0300

    feated: random port binding

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use pak_lab::run;
+use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("failed to bind");
+    let port = listener.local_addr().unwrap().port();
+
+    println!("App running on: 127.0.0.1:{port}");
+    run(listener)?.await
 }
